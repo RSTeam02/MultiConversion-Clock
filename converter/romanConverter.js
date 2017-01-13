6f1b35a5e016a9d25fd87ef4416d61e23abd964b
unknown <sakai.jun01@gmail.com>
2017-01-13 23:06:05 +0100

    tablefix

--- converter/romanConverter.js
+++ converter/romanConverter.js
@@ -1,9 +1,9 @@
 class RomanConverter {
 
     convert(dec) {
-        
+
         //roman numerals 0 - 99
-        let ones = ["\u0020", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+        let ones = ["\u2001", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
         let tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
         let ten = [];
         let one = [];
@@ -16,10 +16,10 @@ class RomanConverter {
             one[i] = dec[i] % 10;
             if (parseInt(dec[dec.length - 1]) === 88) {
                 year88 = 2
-                romArr[i] = ((tens[ten[i]] + ones[one[i]]) + "\u0020\u0020\u0020\u0020\u0020\u0020\u0020").slice(0, 8);
+                romArr[i] = ((tens[ten[i]] + ones[one[i]]) + "\u2001\u2001\u2001\u2001\u2001\u2001\u2001").slice(0, 8);
             } else {
                 year88 = 1;
-                romArr[i] = ((tens[ten[i]] + ones[one[i]]) + "\u0020\u0020\u0020\u0020\u0020\u0020").slice(0, 7);
+                romArr[i] = ((tens[ten[i]] + ones[one[i]]) + "\u2001\u2001\u2001\u2001\u2001\u2001").slice(0, 7);
             }
 
         }
